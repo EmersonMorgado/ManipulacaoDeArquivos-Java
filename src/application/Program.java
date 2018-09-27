e1c6b19a98c2a0c3469e73406bd5d2c6c8a874ae
EmersonMorgado <silvestrevan@gmail.com>
2018-09-27 10:03:07 -0300

    Bloco try with resources

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,45 +1,25 @@
 package application;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
         
         String path = "c:\\temp\\in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
-        
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr); // inicia a partir do fr.
-            
+       
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine(); // quando o arquito chegar no final será retornado null.
+            
             while (line != null) {
                 System.out.println(line);
                 line = br.readLine();
             }
-            
         } 
         catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        }
-        finally{
-            try { //pode ocorrer um erro neste momento quando tentar fechar o arquivo.
-                if (br != null){
-                    br.close();
-                }
-                if (fr != null){
-                    fr.close();
-                }
-            }
-            catch (IOException e){
-                e.printStackTrace();
-            }
         }
     }
     
