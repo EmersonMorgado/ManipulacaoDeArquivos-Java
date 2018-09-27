3b39974e1e19c695f9ca0951d03828ae68ee591a
EmersonMorgado <silvestrevan@gmail.com>
2018-09-27 09:39:11 -0300

    FileReader e BufferedReader - fechando manualmente a stream

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -8,23 +10,37 @@ public class Program {
 
     public static void main(String[] args) {
         
-        File file = new File("c:\\temp\\in.txt");
-        Scanner sc = null;
-        try{
-            sc = new Scanner(file);
-            while (sc.hasNextLine()){
-                System.out.println(sc.nextLine());
+        String path = "c:\\temp\\in.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+        
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr); // inicia a partir do fr.
+            
+            String line = br.readLine(); // quando o arquito chegar no final será retornado null.
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
             }
-        }
-        catch(IOException e){
-            System.out.println("Error: " + e.getMessage() );
+            
+        } 
+        catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
         }
         finally{
-            if(sc != null){
-                sc.close();
+            try { //pode ocorrer um erro neste momento quando tentar fechar o arquivo.
+                if (br != null){
+                    br.close();
+                }
+                if (fr != null){
+                    fr.close();
+                }
+            }
+            catch (IOException e){
+                e.printStackTrace();
             }
         }
-        
     }
     
 }
